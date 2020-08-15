c6f5d92d8b96298fc6deeb983d7a9eda8d653533
abhijitsinghkaloty <62908921+abhijitsinghkaloty@users.noreply.github.com>
2020-08-15 17:55:39 -0400

    This is my first commit message

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ const PREFIX = '.';
 
 bot.on('ready', () =>{
     console.log('Ready!');
+    bot.user.setActivity('CS50!');
 })
 
-bot.on('message', message=>{
+bot.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome-and-goodbye');
+    if (!channel) return;
+    channel.send(`Welcome to CS50, ${member}`);
+});
+
+bot.on('message', message=>{    
 
     let args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]){
+
         case 'welcome':
-            if(message.mentions.members.first() && args[1] === message.mentions.members.first().toString()){
+            if(message.mentions.members.first()){
                 message.channel.send('Welcome to CS50, '+message.mentions.members.first().toString());
             }else{
                 message.channel.send('Welcome to CS50!')
@@ -23,6 +31,32 @@ bot.on('message', message=>{
             break;
         case 'hello':
             message.channel.send('Hello, world!')
+            break;
+        case 'ping':
+            var ping = Date.now() - message.createdTimestamp + " ms";
+
+            if (message.content.startsWith(PREFIX + "ping")) {
+    
+                var ping = Date.now() - message.createdTimestamp + " ms";
+                message.channel.send("Pong! Your ping is `" + `${Date.now() - message.createdTimestamp}` + " ms`.");
+              }
+            break;
+        case 'av':
+            if(message.mentions.users.size){
+                let member = message.mentions.users.first()
+            if(member){
+                const emb = new Discord.MessageEmbed().setImage(member.displayAvatarURL()).setColor(15844367).setTitle(`${member.username}'s avatar:`)
+                message.channel.send(emb);
+                
+            }
+            else{
+                message.channel.send("Sorry none found with that name")
+    
+            }
+            }else{
+                const emb = new Discord.MessageEmbed().setImage(message.author.displayAvatarURL()).setColor(15844367).setTitle(`${message.author.username}'s avatar:`)
+                message.channel.send(emb);
+            }
             break;
         case 'clear':
             if(!args[1]) return message.reply('please specify the number of messages to purge.')
